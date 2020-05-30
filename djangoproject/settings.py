b8a36b11d1ab36239c6c781a416310b892389b9f
satya-lakshmi <gvsslakshmi1998@gmail.com>
2020-05-30 12:20:22 +0530

    third commit

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -196,7 +196,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images/')
